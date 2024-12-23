5f8a8c736b10b3e2dee24deba8b50596f96cf0aa
Wang Yiwen <121547057+yiwen101@users.noreply.github.com>
2024-12-23 12:27:07 +0800

    add offical example for reference and remove wrong take

---
+++ example_src/interfaces/IAxelarGateway.sol
@@ -0,0 +1,166 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+interface IAxelarGateway {
+    /**********\
+    |* Errors *|
+    \**********/
+
+    error NotSelf();
+    error NotProxy();
+    error InvalidCodeHash();
+    error SetupFailed();
+    error InvalidAuthModule();
+    error InvalidTokenDeployer();
+    error InvalidAmount();
+    error InvalidChainId();
+    error InvalidCommands();
+    error TokenDoesNotExist(string symbol);
+    error TokenAlreadyExists(string symbol);
+    error TokenDeployFailed(string symbol);
+    error TokenContractDoesNotExist(address token);
+    error BurnFailed(string symbol);
+    error MintFailed(string symbol);
+    error InvalidSetMintLimitsParams();
+    error ExceedMintLimit(string symbol);
+
+    /**********\
+    |* Events *|
+    \**********/
+
+    event TokenSent(address indexed sender, string destinationChain, string destinationAddress, string symbol, uint256 amount);
+
+    event ContractCall(
+        address indexed sender,
+        string destinationChain,
+        string destinationContractAddress,
+        bytes32 indexed payloadHash,
+        bytes payload
+    );
+
+    event Executed(bytes32 indexed commandId);
+
+    event TokenDeployed(string symbol, address tokenAddresses);
+
+    event ContractCallApproved(
+        bytes32 indexed commandId,
+        string sourceChain,
+        string sourceAddress,
+        address indexed contractAddress,
+        bytes32 indexed payloadHash,
+        bytes32 sourceTxHash,
+        uint256 sourceEventIndex
+    );
+
+    event TokenMintLimitUpdated(string symbol, uint256 limit);
+
+    event OperatorshipTransferred(bytes newOperatorsData);
+
+    event Upgraded(address indexed implementation);
+
+    /********************\
+    |* Public Functions *|
+    \********************/
+
+    function sendToken(
+        string calldata destinationChain,
+        string calldata destinationAddress,
+        string calldata symbol,
+        uint256 amount
+    ) external;
+
+    function callContract(
+        string calldata destinationChain,
+        string calldata contractAddress,
+        bytes calldata payload
+    ) external;
+
+    function isContractCallApproved(
+        bytes32 commandId,
+        string calldata sourceChain,
+        string calldata sourceAddress,
+        address contractAddress,
+        bytes32 payloadHash
+    ) external view returns (bool);
+
+    function validateContractCall(
+        bytes32 commandId,
+        string calldata sourceChain,
+        string calldata sourceAddress,
+        bytes32 payloadHash
+    ) external returns (bool);
+
+    function validateContractCall(
+        bytes32 commandId,
+        string calldata sourceChain,
+        string calldata sourceAddress,
+        bytes calldata payload
+    ) external returns (bool);
+
+    function validateTokenTransfer(
+        bytes32 commandId,
+        string calldata sourceChain,
+        string calldata sourceAddress,
+        address destinationAddress,
+        string calldata denom,
+        uint256 amount
+    ) external returns (bool valid);
+
+    /***********\
+    |* Getters *|
+    \***********/
+
+    function authModule() external view returns (address);
+
+    function governance() external view returns (address);
+
+    function tokenDeployer() external view returns (address);
+
+    function tokenMintLimit(string memory symbol) external view returns (uint256);
+
+    function tokenMintAmount(string memory symbol) external view returns (uint256);
+
+    function tokenDenom(string memory symbol) external view returns (string memory);
+
+    function tokenSymbol(string memory denom) external view returns (string memory);
+
+    function allTokensFrozen() external view returns (bool);
+
+    function implementation() external view returns (address);
+
+    function tokenAddresses(string memory symbol) external view returns (address);
+
+    function tokenFrozen(string memory symbol) external view returns (bool);
+
+    function isCommandExecuted(bytes32 commandId) external view returns (bool);
+
+    function adminEpoch() external view returns (uint256);
+
+    function adminThreshold(uint256 epoch) external view returns (uint256);
+
+    function admins(uint256 epoch) external view returns (address[] memory);
+
+    /*******************\
+    |* Admin Functions *|
+    \*******************/
+
+    function setTokenMintLimits(string[] calldata symbols, uint256[] calldata limits) external;
+
+    function upgrade(
+        address newImplementation,
+        bytes32 newImplementationCodeHash,
+        bytes calldata setupParams
+    ) external;
+
+    /**********************\
+    |* External Functions *|
+    \**********************/
+
+    function setup(bytes calldata params) external;
+
+    function execute(bytes calldata input) external;
+
+    // Can only be called by governance
+    function deployToken(bytes calldata params, bytes32) external;
+}
--- xrp-loc-project/migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-// migrations/2_deploy_contracts.js
-const Lock = artifacts.require("Lock");
-
-module.exports = function (deployer) {
-  deployer.deploy(Lock);
-};
