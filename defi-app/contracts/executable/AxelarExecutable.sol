5af3ab3b140150d84e691d1142ab32ad53380c2d
Wang Yiwen <121547057+yiwen101@users.noreply.github.com>
2024-12-23 12:41:09 +0800

    init new project

---
+++ defi-app/contracts/executable/AxelarExecutable.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import { IAxelarGateway } from '../interfaces/IAxelarGateway.sol';
+import { IAxelarExecutable } from '../interfaces/IAxelarExecutable.sol';
+
+contract AxelarExecutable is IAxelarExecutable {
+    IAxelarGateway public immutable gateway;
+
+    constructor(address gateway_) {
+        if (gateway_ == address(0)) revert InvalidAddress();
+
+        gateway = IAxelarGateway(gateway_);
+    }
+
+    function execute(
+        bytes32 commandId,
+        string calldata sourceChain,
+        string calldata sourceAddress,
+        bytes calldata payload
+    ) external {
+        if (!gateway.validateContractCall(commandId, sourceChain, sourceAddress, payload))
+            revert NotApprovedByGateway();
+
+        _execute(sourceChain, sourceAddress, payload);
+    }
+
+    function _execute(
+        string calldata sourceChain,
+        string calldata sourceAddress,
+        bytes calldata payload
+    ) internal virtual {}
+}
