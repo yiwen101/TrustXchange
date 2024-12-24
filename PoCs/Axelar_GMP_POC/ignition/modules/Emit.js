b30b0d0c894e953eb47bc90a942c4b8b453d3fda
Wang Yiwen <121547057+yiwen101@users.noreply.github.com>
2024-12-25 00:37:09 +0800

    Revert "Add emit events poc contract, and functional backend for detecting for events"
    This reverts commit 7257588ba304403d1420f48268ed38b0c3e2f2a0.

--- PoCs/Axelar_GMP_POC/ignition/modules/Emit.js
+++
@@ -1,5 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("EmitModule", (m) => {
-    const emit = m.contract("Emit", []);
-});
--- backend/src/main/java/com/trustXchange/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.trustXchange;
-
-public class Constants {
-    
-}
