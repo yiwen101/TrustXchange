13d41df07425565a7f853cd6bb8db4dc70728be5
Wang Yiwen <121547057+yiwen101@users.noreply.github.com>
2024-12-29 19:17:24 +0800

    init defi and axelarGateway

---
+++ deFi/const.js
@@ -0,0 +1,2 @@
+const priceOracleAddress = '0xcf0d31a6806AE62A1Ec9bc05606874870EE24b63'
+const xrpEvmSideChainUrl = "https://rpc-evm-sidechain.xrpl.org"
---
+++ deFi/todo.md
@@ -0,0 +1,4 @@
+save info for XRPFIATBinancePriceOracle.sol
+write code for liquidity pool depends on priceOracle
+deploy the pool
+attemp to interact
