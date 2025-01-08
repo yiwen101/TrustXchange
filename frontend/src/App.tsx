d64c6be81e30271f790849aa4282e0bc37d0396c
Wang Yiwen <121547057+yiwen101@users.noreply.github.com>
2025-01-08 23:55:33 +0800

    removed unused import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,12 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
 import ClientLoader from './InitLoader';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import WalletComponent from './components/WalletComponent';
+import { BrowserRouter} from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <RecoilRoot>
       <BrowserRouter>
-        <Routes>
-          <Route path="/wallet" element={<WalletComponent />} />
-          {/* Keep other routes */}
-        </Routes>
         <ClientLoader />
       </BrowserRouter>
     </RecoilRoot>
