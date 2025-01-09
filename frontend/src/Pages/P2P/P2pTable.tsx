51e65e1e9ed7e3710d74a7f486c7d0ec0802ff4d
Wang Yiwen <121547057+yiwen101@users.noreply.github.com>
2025-01-09 20:20:27 +0800

    refractor code

--- frontend/src/Pages/P2P/P2pTable.tsx
+++ frontend/src/Pages/P2P/P2pTable.tsx
@@ -1,14 +1,12 @@
-import { Box, Chip, TextField, Grid, MenuItem, Select, FormControl, InputLabel, Stack, Button } from "@mui/material";
+import { Box, Chip, Button } from "@mui/material";
 import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
-import React, { useState, useEffect } from "react";
-import { useP2pValues, useP2pActions } from "../../hooks/useP2pLendingState";
+import React from "react";
 import {
-    P2pBorrowingRequest,
-    P2pLendingRequest,
+  P2pBorrowingRequest,
+  P2pLendingRequest,
 } from "../../api/backend/types/p2pTypes";
 
 type RequestType = P2pBorrowingRequest | P2pLendingRequest;
-
 
 function renderType(type: 'Borrow' | 'Lend') {
     const colors: { [key: string]: 'default' | 'primary' | 'secondary' | 'error' | 'success' | 'warning' } = {
@@ -60,33 +58,13 @@ const borrowingRequestColumns: GridColDef[] = [
 ];
 
 
+interface P2pTableProps {
+    rows: RequestType[];
+}
 
-const P2pTable: React.FC = () => {
-    const { allRequests } = useP2pValues();
-    const { fetchAllRequests } = useP2pActions();
+const P2pTable: React.FC<P2pTableProps> = ({ rows }) => {
 
-    useEffect(() => {
-        fetchAllRequests();
-    }, [fetchAllRequests]);
-
-    const myName = 'Bob';
-    const [showOnlyMe, setShowOnlyMe] = useState<string>('any');
-    const [requestTypeFilter, setRequestTypeFilter] = useState<string>("borrow/lend");
-    const [statusFilter, setStatusFilter] = useState<string>('Not Filled/Partial Filled');
-    const [durationCondition, setDurationCondition] = useState<string>('any');
-    const [durationFilter, setDurationFilter] = useState<string>('');
-    const [interestRateCondition, setInterestRateCondition] = useState<string>('any');
-    const [interestRateFilter, setInterestRateFilter] = useState<string>('');
-    const [collateralCondition, setCollateralCondition] = useState<string>('any');
-    const [collateralFilter, setCollateralFilter] = useState<string>('');
-
-    // Combine requests into a single array for filtering
-    const requestRows: RequestType[] = allRequests
-    ? [...allRequests.borrowRequests, ...allRequests.lendRequests]
-    : [];
-
-
-    const filteredRows = requestRows.map(row => {
+    const transformedRows = rows.map(row => {
         const isBorrowRequest = (row as P2pBorrowingRequest).borrower !== undefined;
         const borrower = isBorrowRequest ? (row as P2pBorrowingRequest).borrower : '_';
         const lender = isBorrowRequest ? '_' : (row as P2pLendingRequest).lender;
@@ -123,7 +101,7 @@ const P2pTable: React.FC = () => {
 
 
         return {
-            id: isBorrowRequest ? (row as P2pBorrowingRequest).requestId : (row as P2pLendingRequest).requestId,
+            id: isBorrowRequest ? (row as P2pBorrowingRequest).requestId*2 : (row as P2pLendingRequest).requestId*2+1,
             type: type,
             status: status,
             amount: amountDisplay,
@@ -133,204 +111,26 @@ const P2pTable: React.FC = () => {
             borrower: borrower,
             lender: lender
         }
-    }).filter(row => {
-        return (
-            (showOnlyMe === "any" ||
-                (showOnlyMe === "true" && (row.borrower === myName || row.lender === myName)) ||
-                (showOnlyMe === "false" && row.borrower !== myName && row.lender !== myName)) &&
-            (requestTypeFilter === "borrow/lend" ||
-                !row.type.toLowerCase().includes(requestTypeFilter.toLowerCase())) &&
-            (statusFilter === "any" ||
-                (statusFilter === "Not Filled/Partial Filled" && (row.status === "Not Filled" || row.status === "Partial Filled")) ||
-                row.status.toLowerCase().includes(statusFilter.toLowerCase())) &&
-            (durationCondition === "any" ||
-                durationFilter === "" ||
-                (durationCondition === "greater" &&
-                    row.duration > parseInt(durationFilter)) ||
-                (durationCondition === "less" &&
-                    row.duration < parseInt(durationFilter)) ||
-                (durationCondition === "equal" &&
-                    row.duration === parseInt(durationFilter))) &&
-            (interestRateCondition === "any" ||
-                interestRateFilter === "" ||
-                (interestRateCondition === "greater" &&
-                    row.interestRate > parseFloat(interestRateFilter)) ||
-                (interestRateCondition === "less" &&
-                    row.interestRate < parseFloat(interestRateFilter)) ||
-                (interestRateCondition === "equal" &&
-                    row.interestRate === parseFloat(interestRateFilter))) &&
-            (collateralCondition === "any" ||
-                collateralFilter === "" ||
-                (collateralCondition === "greater" &&
-                    row.collateral > parseFloat(collateralFilter)) ||
-                (collateralCondition === "less" &&
-                    row.collateral < parseFloat(collateralFilter)) ||
-                (collateralCondition === "equal" &&
-                    row.collateral === parseFloat(collateralFilter)))
-        );
-    });
+    })
 
 
-    return (
-        <Stack display="flex" flexDirection="column" justifyContent="flex-start" p={2}>
-            <Grid container spacing={2} mb={2} style={{ marginTop: '10px' }}>
-                <Grid item xs={2}>
-                    <FormControl fullWidth variant="outlined" size="small">
-                        <InputLabel>Type</InputLabel>
-                        <Select
-                            label="Borrow/Lend"
-                            value={requestTypeFilter}
-                            onChange={(e) => setRequestTypeFilter(e.target.value)}
-                        >
-                            <MenuItem value="borrow/lend">Borrow/Lend</MenuItem>
-                            <MenuItem value="Borrow">Borrow</MenuItem>
-                            <MenuItem value="Lend">Lend</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={2}>
-                    <FormControl fullWidth variant="outlined" size="small">
-                        <InputLabel>Status</InputLabel>
-                        <Select
-                            label="Status"
-                            value={statusFilter}
-                            onChange={(e) => setStatusFilter(e.target.value)}
-                        >
-                            <MenuItem value="any">Any</MenuItem>
-                             <MenuItem value="Not Filled/Partial Filled">Not Filled/Partial Filled</MenuItem>
-                            <MenuItem value="Not Filled">Not Filled</MenuItem>
-                            <MenuItem value="Partial Filled">Partial Filled</MenuItem>
-                            <MenuItem value="Filled">Filled</MenuItem>
-                            <MenuItem value="Cancelled">Cancelled</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={2}>
-                    <FormControl fullWidth variant="outlined" size="small">
-                        <InputLabel>Involve Me</InputLabel>
-                        <Select
-                            label="My Contract"
-                            value={showOnlyMe}
-                            onChange={(e) => setShowOnlyMe(e.target.value)}
-                        >
-                            <MenuItem value='any'>Any</MenuItem>
-                            <MenuItem value='true'>True</MenuItem>
-                            <MenuItem value='false'>False</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={3}>
-                    <FormControl fullWidth variant="outlined" size="small">
-                        <InputLabel>Duration</InputLabel>
-                        <Select
-                            label="Duration"
-                            value={durationCondition}
-                            onChange={(e) => setDurationCondition(e.target.value)}
-                        >
-                            <MenuItem value="any">Any</MenuItem>
-                            <MenuItem value="greater">Greater than</MenuItem>
-                            <MenuItem value="less">Less than</MenuItem>
-                            <MenuItem value="equal">Equal to</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={3}>
-                    <TextField
-                        label="Duration Value"
-                        variant="outlined"
-                        size="small"
-                        fullWidth
-                        value={durationFilter}
-                        onChange={(e) => setDurationFilter(e.target.value)}
-                        disabled={durationCondition === 'any'}
-                        InputProps={{
-                            style: {
-                                backgroundColor: durationCondition === 'any' ? '#f0f0f0' : 'inherit',
-                            },
-                        }}
-                    />
-                </Grid>
-                <Grid item xs={3}>
-                    <FormControl fullWidth variant="outlined" size="small">
-                        <InputLabel>Interest Rate</InputLabel>
-                        <Select
-                            label="Interest Rate"
-                            value={interestRateCondition}
-                            onChange={(e) => setInterestRateCondition(e.target.value)}
-                        >
-                            <MenuItem value="any">Any</MenuItem>
-                            <MenuItem value="greater">Greater than</MenuItem>
-                            <MenuItem value="less">Less than</MenuItem>
-                            <MenuItem value="equal">Equal to</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={3}>
-                    <TextField
-                        label="Interest Rate Value"
-                        variant="outlined"
-                        size="small"
-                        fullWidth
-                        value={interestRateFilter}
-                        onChange={(e) => setInterestRateFilter(e.target.value)}
-                        disabled={interestRateCondition === 'any'}
-                        InputProps={{
-                            style: {
-                                backgroundColor: interestRateCondition === 'any' ? '#f0f0f0' : 'inherit',
-                            },
-                        }}
-                    />
-                </Grid>
-                <Grid item xs={3}>
-                    <FormControl fullWidth variant="outlined" size="small">
-                        <InputLabel>Collateral</InputLabel>
-                        <Select
-                            label="Collateral"
-                            value={collateralCondition}
-                            onChange={(e) => setCollateralCondition(e.target.value)}
-                        >
-                            <MenuItem value="any">Any</MenuItem>
-                            <MenuItem value="greater">Greater than</MenuItem>
-                            <MenuItem value="less">Less than</MenuItem>
-                            <MenuItem value="equal">Equal to</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={3}>
-                    <TextField
-                        label="Collateral Value"
-                        variant="outlined"
-                        size="small"
-                        fullWidth
-                        value={collateralFilter}
-                        onChange={(e) => setCollateralFilter(e.target.value)}
-                        disabled={collateralCondition === 'any'}
-                        InputProps={{
-                            style: {
-                                backgroundColor: collateralCondition === 'any' ? '#f0f0f0' : 'inherit',
-                            },
-                        }}
-                    />
-                </Grid>
-            </Grid>
-            <DataGrid
-                rows={filteredRows}
-                columns={borrowingRequestColumns}
-                getRowClassName={(params) =>
-                    params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd'
-                }
-                initialState={{
-                    pagination: { paginationModel: { pageSize: 20 } },
-                }}
-                pageSizeOptions={[10, 20, 50]}
-                disableColumnResize
-                density="compact"
-            />
-            <Button variant="contained" color="primary">
-                Submit New Lending/Borrowing Request
-            </Button>
-        </Stack>
-    );
+  return (
+    <Box>
+      <DataGrid
+        rows={transformedRows}
+        columns={borrowingRequestColumns}
+        getRowClassName={(params) =>
+          params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd'
+        }
+        initialState={{
+          pagination: { paginationModel: { pageSize: 20 } },
+        }}
+        pageSizeOptions={[10, 20, 50]}
+        disableColumnResize
+        density="compact"
+      />
+    </Box>
+  );
 };
 
 export default P2pTable;
