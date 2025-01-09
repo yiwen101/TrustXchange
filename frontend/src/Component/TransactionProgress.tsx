9465b7f63508dff1da161c5d882614a8f7f9a0c7
Wang Yiwen <121547057+yiwen101@users.noreply.github.com>
2025-01-09 12:23:27 +0800

    add is approved logic

--- frontend/src/Component/TransactionProgress.tsx
+++ frontend/src/Component/TransactionProgress.tsx
@@ -7,8 +7,10 @@ import CircularProgress from '@mui/material/CircularProgress';
 import {USDC_issuer} from "../const";
 import { useCurrentGMPCallState } from '../hooks/useCurrnetGMPCallState';
 import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
+import InfoIcon from '@mui/icons-material/Info';
 
 const ApproveTransaction = () => {
+    const [isApproved, setIsApproved] = React.useState(false);
     const {xrplTransaction, evmTransaction} = useCurrentGMPCallState();
   return (
     <Paper elevation={3} sx={{ maxWidth: 600, padding: 3, margin: '20px auto' }}>
@@ -16,7 +18,8 @@ const ApproveTransaction = () => {
         Approve the Transaction
       </Typography>
 
-      <Box sx={{ marginBottom: 3 }}>
+      <Box sx={{ marginBottom: 3, color: !isApproved ? 'grey.500' : 'inherit',
+        opacity: !isApproved ? 0.6 : 1,}}>
         <Typography variant="subtitle1" gutterBottom>
           Sign and create GMP call
         </Typography>
@@ -31,11 +34,19 @@ const ApproveTransaction = () => {
             </Typography>
           </Box>
         )}
-        {!xrplTransaction && (
+        {isApproved&&!xrplTransaction && (
             <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
                 <CircularProgress size={20} />
                 <Typography variant="body1" sx={{ fontWeight: 500 }}>
                     Making GMP Call to Multisig Wallet on XRPL Testnet
+                </Typography>
+            </Box>
+        )}
+        {!isApproved && (
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                <HourglassBottomIcon />
+                <Typography variant="body1" sx={{ fontWeight: 500 }}>
+                    Waiting for Approval to Sign and Create GMP Call
                 </Typography>
             </Box>
         )}
@@ -103,29 +114,30 @@ const ApproveTransaction = () => {
           <Typography variant="body1">Send</Typography>
           <Typography variant="body1">90 XRP</Typography>
         </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Typography variant="body1">Receive</Typography>
-          <Typography variant="body1">90 XRP</Typography>
-        </Box>
       </Box>
 
       <Divider sx={{ my: 2 }} />
 
       {/* Fee and Time */}
       <Box sx={{ marginBottom: 3 }}>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <Tooltip title="Bridge transfer fees cover network costs." placement="top">
-            <Typography variant="body2">Bridge Transfer Fee</Typography>
-          </Tooltip>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Tooltip title="Bridge transfer fees cover network costs." placement="top">
+          <Typography variant="body2">Bridge Transfer Fee</Typography>
+        </Tooltip>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
           <Typography variant="body2">~ 0 XRP</Typography>
+          <Tooltip title="We may change this behaviour in production" placement="right">
+            <InfoIcon fontSize="small" sx={{ ml: 0.5, cursor: 'pointer' }} />
+          </Tooltip>
         </Box>
+      </Box>
         <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
           <Typography variant="body2">Estimated time needed</Typography>
           <Typography variant="body2">~ 30 seconds - 3 minutes</Typography>
         </Box>
       </Box>
 
-      <Button variant="contained" color="primary" fullWidth>
+      <Button variant="contained" color="primary" fullWidth onClick={() => setIsApproved(true)} disabled={isApproved}>
         Approve
       </Button>
     </Paper>
